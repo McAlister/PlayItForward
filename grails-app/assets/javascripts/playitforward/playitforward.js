91570c6dc2b4c321a04c648bd8f884163dfb9fe6
Simone Aiken <saiken@Simones-MacBook-Pro.local>
2017-01-01 18:35:30 -0700

    Gradle stub application

---
+++ grails-app/assets/javascripts/playitforward/playitforward.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /playitforward/core/playitforward.core
+//= require /playitforward/index/playitforward.index
+
+angular.module("playitforward", [
+        "playitforward.core",
+        "playitforward.index"
+    ]);
---
+++ grails-app/init/playitforward/BootStrap.groovy
@@ -0,0 +1,9 @@
+package playitforward
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
