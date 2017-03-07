7b207d80a63fc25a1d8dab976dece0b29c7ac490
Simone Aiken <saiken@ulfheim.net>
2017-03-06 19:30:21 -0700

    update of event codes for known coded

---
+++ scripts/dataLoad/2.0.0/01_update_events.sql
@@ -0,0 +1,14 @@
+
+
+update event set event_code = 'gplou17' where name = 'GP Louisville';
+
+update event set event_code = 'gpsj17' where name = 'GP San Jose';
+
+update event set event_code = 'gppit17' where name = 'GP Pittsburgh';
+
+update event set event_code = 'gpvan17' where name = 'GP Vancouver';
+
+update event set event_code = 'gpnja17' where name = 'GP New Jersey Standard';
+
+update event set event_code = 'gporl17' where name = 'GP Orlando';
+
