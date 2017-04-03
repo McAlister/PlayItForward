0e88b56d8c86d09c9eb9f1b1db3f1fb40d9f69b7
Simone Aiken <saiken@ulfheim.net>
2017-04-02 22:05:35 -0600

    Make parsestandings get start/end dates from the database

--- scripts/parseStandings.pl
+++ scripts/parseStandings.pl
@@ -17,57 +17,34 @@
 	}
 
     my $pass = $ARGV[2];
-    #my $dbh = DBI->connect("DBI:Pg:dbname=magicmom;host=localhost", "magic", $pass);
-    my $dbh = DBI->connect("DBI:Pg:dbname=magicmom;host=magicmom.c00icap1mfll.us-west-2.rds.amazonaws.com", "magic", $pass);
+    my $dbh = DBI->connect("DBI:Pg:dbname=magicmom;host=localhost", "magic", $pass);
+    #my $dbh = DBI->connect("DBI:Pg:dbname=magicmom;host=magicmom.c00icap1mfll.us-west-2.rds.amazonaws.com", "magic", $pass);
 	my $abbr = $ARGV[0];
 	my $round = $ARGV[1];
 
-	my $day1 = "07";
-	my $day2 = "08";
-	my $month = "01";
     my $prefix = "";
-	if ($abbr eq "gppit17") {
+    if ($abbr eq "gpvan17") {
 
-	    $month = "02";
-	    $day1 = "11";
-	    $day2 = "12";
-	}
-	elsif ($abbr eq "gpsj17") {
-
-	    $day1 = "28";
-	    $day2 = "29";
-	}
-	elsif ($abbr eq "gpvan17") {
-
-        $prefix = "0";
-	    $month = "02";
-	    $day1 = "18";
-	    $day2 = "19";
-	}
-    elsif ($abbr eq "gpnj17") {
-
-	    $month = "03";
-	    $day1 = "11";
-	    $day2 = "12";
+          $prefix = "0";
     }
 
-	my $url = "http://magic.wizards.com/en/events/coverage/$abbr/round-$prefix" . $round . "-standings-2017-$month-$day1";
-	my $file = "round-" . $round . "-standings-2017-$month-$day1";
-	if ( $round > 9 )
-	{
-		$url = "http://magic.wizards.com/en/events/coverage/$abbr/round-" . $round . "-standings-2017-$month-$day2";
-		$file = "round-" . $round . "-standings-2017-$month-$day2";
-	}
+    my ($eventID, $start, $end) = &getEventID($abbr);
 
-	unless ( -e $file )
-	{
+    my $url = "http://magic.wizards.com/en/events/coverage/$abbr/round-$prefix" . $round . "-standings-$start";
+    my $file = "round-" . $round . "-standings-2017-$start";
+    if ( $round > 9 ) {       
+
+            $url = "http://magic.wizards.com/en/events/coverage/$abbr/round-" . $round . "-standings-$end";
+            $file = "round-" . $round . "-standings-$end";
+    }
+
+	unless ( -e $file ) {
+
         print "Curling: curl $url > $file\n\n";
 		`curl $url > $file`;
 	}
 
 	open( INPUT, "<$file" ) or die "\nCannot open $file.\n\n";
-
-    my $eventID = &getEventID($abbr);
 
     my $count = 0;
 	while ( my $rankLine = <INPUT> )
@@ -88,7 +65,7 @@
                 . "VALUES \n"
                 . "(nextval('hibernate_sequence'), 0, ?, ?, ?, ?, ?, ?, false);";
 
-            #print "SQL:\n\n$sql\n\n";
+            #print "SQL:\n\n$sql\n\n $eventID, $name, $omw, $points, $rank, $round";
 
             my $qh = $dbh->prepare($sql) or die "Cannot Prepare Statement.";
             $qh->bind_param(1, $eventID);
@@ -132,16 +109,12 @@ sub getValue() {
 
 sub getEventID() {
 
-    my $eventCode = $_[0];
+        my $eventCode = $_[0];
 
-    my $qh = $dbh->prepare("Select id from event where event_code = ?") or die "Cannot Prepare Statement.";
-    $qh->bind_param(1, $abbr);
+        my $qh = $dbh->prepare("Select id, start_date, end_date from event where event_code = ?") or die "Cannot Prepare Statement.";
+        $qh->bind_param(1, $abbr);
 
-    $qh->execute() or die "Cannot get Event ID.";
+        $qh->execute() or die "Cannot get Event ID.";
 
-    my $id = $qh->fetchrow_array();
-
-    print "EVENT ID IS: $id\n\n";
-
-    return $id;
+        return $qh->fetchrow_array();
 }
