4f55d04b13ca866640639f85d05866acd524ac39
Michael Driscoll <michael@ulfheim.net>
2017-04-04 19:42:10 -0600

    Get mailflow actually working

--- grails-app/services/playitforward/EmailService.groovy
+++ grails-app/services/playitforward/EmailService.groovy
@@ -2,6 +2,7 @@ package playitforward
 
 import grails.plugin.awssdk.ses.AmazonSESService
 import grails.transaction.Transactional
+import grails.util.Environment
 
 @Transactional
 class EmailService {
@@ -11,14 +12,17 @@ class EmailService {
 
     def sendPasswordReset(User user) {
         def to = user.username
-        def subject = 'Change your playitforward password'
-        def body = '<p>Hello ${to.encodeAsHTML()}!' +
-                '<p>Please click <a href="http://www.playitforward-magic.org/#/setPass' +
-                '?user=${to.encodeAsURL()}&resetKey=${user.resetKey.encodeAsUrl()}">' +
-                'this link</a> to set or change your password.'
+        def subject = 'Set or change your playitforward password'
+        def host = Environment.current == Environment.PRODUCTION ? 'www.playitforward-magic.org' : 'localhost:8080'
+        def url = "http://${host}/#/setPass?user=${URLEncoder.encode(to, 'utf-8')}" +
+                "&resetKey=${URLEncoder.encode(user.resetKey, 'utf-8')}"
+        def body = "<p>Hello playitforward user!\n" +
+                "<p>Please click <a href='${url}'>this link</a> to set or change your password.\n"
 
         int result = amazonSESService.send(to, subject, body, sender)
-        System.console().println("Sent password reset email to ${to} with key ${user.resetKey} and result ${result}")
-        return result
+        if (result != 1) {
+            throw new RuntimeException("Unable to send mail to ${to}, result: ${result}")
+        }
+        System.console().println("Sent password reset email to ${to} with key ${user.resetKey}")
     }
 }
