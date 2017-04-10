b900437e05e00e5d2ac396e20c99c3247518146f
Simone Aiken <saiken@ulfheim.net>
2017-04-09 21:11:58 -0600

    Update event codes alter

---
+++ scripts/dataLoad/3.0.0/02_update_events.sql
@@ -0,0 +1,8 @@
+
+
+UPDATE event SET event_code = 'gpric17' WHERE name = 'GP Richmond';
+
+UPDATE event SET event_code = 'gpmon17' WHERE name = 'GP Montreal';
+
+UPDATE event SET event_code = 'gpoma17' WHERE name = 'GP Omaha';
+
