89180176c15164fef2baa398208e1d80cd7e60ca
Michael Driscoll <michael@ulfheim.net>
2017-04-14 20:45:06 -0600

    Set custom Cache-Control of 1 year (default) on every S3 object

--- grails-app/services/playitforward/S3Service.groovy
+++ grails-app/services/playitforward/S3Service.groovy
@@ -1,5 +1,6 @@
 package playitforward
 
+import com.amazonaws.services.s3.model.PutObjectRequest
 import grails.plugin.awssdk.s3.AmazonS3Service
 import grails.transaction.Transactional
 
@@ -9,12 +10,20 @@ class S3Service {
     AmazonS3Service amazonS3Service
     def grailsApplication
 
-    def uploadFile(String path, File file) {
+    void uploadFile(String path, File file, int maxAge=365*24*60*60) {
         def bucket = grailsApplication.config.getProperty('bucket') as String
-        amazonS3Service.storeFile(bucket, path, file)
+        try {
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, path, file)
+            putObjectRequest.putCustomRequestHeader(
+                    'Cache-Control', "max-age=${maxAge}")
+            amazonS3Service.client.putObject(putObjectRequest)
+        } catch (Exception exception) {
+            log.warn 'An exception was caught while storing file', exception
+            throw exception
+        }
     }
 
-    def downloadFile(String path, File file) {
+    File downloadFile(String path, File file) {
         def bucket = grailsApplication.config.getProperty('bucket') as String
         amazonS3Service.getFile(bucket, path, file.getAbsolutePath())
     }
