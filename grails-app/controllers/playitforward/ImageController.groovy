0f1790e42285cdbceb778e80cb4c40ab44a1cee5
Simone Aiken <saiken@ulfheim.net>
2017-05-18 20:45:06 -0600

    Adding unit test to ImageController

--- grails-app/controllers/playitforward/ImageController.groovy
+++ grails-app/controllers/playitforward/ImageController.groovy
@@ -4,24 +4,33 @@ import grails.plugin.springsecurity.annotation.Secured
 import org.apache.commons.io.IOUtils
 
 class ImageController {
-    CameoService cameoService
-    static responseFormats = ['json', 'xml']
 
-    def index() { }
+    CameoService cameoService;
+    static responseFormats = ['json', 'xml'];
+
+    def index() {
+
+        // Empty Block
+    }
 
     @Secured(['permitAll'])
     def generateCameo() {
-        File input = File.createTempFile("cameo-upload", ".png")
-        File output = File.createTempFile("cameo-output", ".png")
+
+        File input = File.createTempFile("cameo-upload", ".png");
+        File output = File.createTempFile("cameo-output", ".png");
+
         try {
-            IOUtils.copy(request.inputStream, new FileOutputStream(input))
-            println "Input:${input} Output:${output}"
-            cameoService.makeCameo(input, output)
-            render file: new FileInputStream(output), contentType: 'image/png'
+
+            IOUtils.copy(request.inputStream, new FileOutputStream(input));
+            println "Input:${input} Output:${output}";
+            cameoService.makeCameo(input, output);
+
+            render file: new FileInputStream(output), contentType: 'image/png';
         }
         finally {
-            input.delete()
-            output.delete()
+
+            input.delete();
+            output.delete();
         }
     }
 }
--- src/test/groovy/playitforward/ImageControllerSpec.groovy
+++ src/test/groovy/playitforward/ImageControllerSpec.groovy
@@ -1,6 +1,7 @@
 package playitforward
 
 import grails.test.mixin.TestFor
+import org.grails.plugins.testing.GrailsMockMultipartFile
 import spock.lang.Specification
 
 /**
@@ -9,14 +10,43 @@ import spock.lang.Specification
 @TestFor(ImageController)
 class ImageControllerSpec extends Specification {
 
+    CameoService mockCameo = Mock();
+
     def setup() {
+
+        controller.setCameoService(mockCameo);
     }
 
     def cleanup() {
+
+        // Empty Block
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "test generateCameo"() {
+
+        given:
+            ClassLoader classLoader = getClass().getClassLoader();
+            File file = new File(classLoader.getResource("images/Zyla.png").getFile());
+            byte[] input = file.getBytes();
+
+            def multipartFile = new GrailsMockMultipartFile('inputStream',
+                    'Zyla.png', 'image/jpeg', input);
+            request.addFile(multipartFile);
+
+        when:"calling generateCameo()"
+            controller.generateCameo();
+
+        then:"Cameo Made"
+            1 * mockCameo.makeCameo(_ as File, _ as File);
+            response.status == 200;
+    }
+
+    void "noop index"() {
+
+        when:"calling index"
+            controller.index();
+
+        then:"noop"
+            true;
     }
 }
