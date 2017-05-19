0bfb772bac316bfbc89c6f99a73f2b10cda6cbd1
Simone Aiken <saiken@ulfheim.net>
2017-05-18 18:50:24 -0600

    Wrote unit tests for UserController

--- grails-app/controllers/playitforward/UserController.groovy
+++ grails-app/controllers/playitforward/UserController.groovy
@@ -6,37 +6,46 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 @Secured(['ROLE_ADMIN'])
 class UserController {
-	static responseFormats = ['json', 'xml']
-    static allowedMethods = []
-    def emailService
+
+	static responseFormats = ['json', 'xml'];
+    static allowedMethods = [];
+    def emailService;
 
     def index() {
+        // Empty Block
     }
 
     @Secured(['permitAll'])
     def sendResetEmail(String username) {
-        username = username.toLowerCase()
-        def user = User.findByUsername(username)
+
+        username = username.toLowerCase();
+        def user = User.findByUsername(username);
+
         if (user != null) {
-            user.makeResetKey()
-            emailService.sendPasswordReset(user)
-            user.save(flush: true)
+
+            user.makeResetKey();
+            emailService.sendPasswordReset(user);
+            user.save(flush: true);
         }
-        respond([message: 'email sent if user exists'])
+
+        respond([message: 'email sent if user exists']);
     }
 
     @Secured(['permitAll'])
     def setNewPassword(String username) {
-        username = username.toLowerCase()
-        def password = params.password ?: request.JSON?.password
-        def resetKey = params.key ?: request.JSON?.key
-        def user = User.findByUsername(username)
+
+        username = username.toLowerCase();
+        def password = params.password ?: request.JSON?.password;
+        def resetKey = params.key ?: request.JSON?.key;
+        def user = User.findByUsername(username);
+
         if (user.resetKey != resetKey) {
-            return respond([status: 403], [message: 'Incorrect key, be sure to use the latest reset email'])
+            return respond([status: 403], [message: 'Incorrect key, be sure to use the latest reset email']);
         }
-        user.password = password
-        user.resetKey = null
-        user.save(flush: true)
-        respond([message: 'success'])
+
+        user.password = password;
+        user.resetKey = null;
+        user.save(flush: true);
+        respond([message: 'success']);
     }
 }
--- src/test/groovy/playitforward/UserControllerSpec.groovy
+++ src/test/groovy/playitforward/UserControllerSpec.groovy
@@ -2,21 +2,79 @@ package playitforward
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import grails.test.mixin.*
+import grails.test.mixin.domain.DomainClassUnitTestMixin
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(UserController)
+@Mock(User)
+@TestMixin(DomainClassUnitTestMixin)
 class UserControllerSpec extends Specification {
 
+    EmailService mockEmailService = Mock();
+
     def setup() {
+
+        controller.setEmailService(mockEmailService);
     }
 
     def cleanup() {
+
+        // Empty Block
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "test set new password"() {
+
+        mockDomain(User, [
+                [username: "saiken@ulfheim.net", password: "testing", resetKey: "test key"]
+        ]);
+
+        when:"Call set new password with no reset key"
+            controller.setNewPassword("saiken@ulfheim.net");
+
+        then:"wrong reset key error"
+            response.status == 403;
+
+        when:"correct reset key sent"
+            response.reset();
+            controller.params.key = "test key";
+            controller.params.password = "explorerReporting";
+            controller.setNewPassword("saiken@ulfheim.net");
+            controller.params.clear();
+
+        then:"Success"
+            response.status == 200;
+            response.text == "{\"message\":\"success\"}";
+            User user = User.findByUsername("saiken@ulfheim.net");
+            user.password.equals("explorerReporting");
+    }
+
+    void "test user does not exist"() {
+
+        when:"Call send reset with imaginary user"
+            controller.sendResetEmail("saiken@ulfheim.net");
+
+        then:"no op"
+            response.status == 200;
+            response.text == "{\"message\":\"email sent if user exists\"}";
+    }
+
+    void "test user exists"() {
+
+        mockDomain(User, [
+                [username: "saiken@ulfheim.net", password: "testing"]
+        ]);
+
+        when:"Call reset with real user"
+            controller.sendResetEmail("saiken@ulfheim.net");
+            User user = User.findByUsername("saiken@ulfheim.net");
+
+        then:"no op"
+            1 * mockEmailService.sendPasswordReset(_ as User);
+            response.status == 200;
+            user.resetKey != null;
+            response.text == "{\"message\":\"email sent if user exists\"}";
     }
 }
